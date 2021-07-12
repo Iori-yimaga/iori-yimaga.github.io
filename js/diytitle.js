d6611f255d827d58b5ed6865fc905cec16170a5c
Iori-yimaga <964302865@qq.com>
2021-07-12 14:12:46 +0800

    Site updated: 2021-07-12 14:12:46

--- js/diytitle.js
+++ js/diytitle.js
@@ -1,18 +1,1 @@
-//动态标题
-var OriginTitile = document.title;
-var titleTime;
-document.addEventListener('visibilitychange', function () {
-    if (document.hidden) {
-        //离开当前页面时标签显示内容
-        document.title = 'w(ﾟДﾟ)w 你在看哪里？';
-        clearTimeout(titleTime);
-    }
-    else {
-        //返回当前页面时标签显示内容
-        document.title = '♪(^∇^*)继续继续！' + OriginTitile;
-        //两秒后变回正常标题
-        titleTime = setTimeout(function () {
-            document.title = OriginTitile;
-        }, 2000);
-    }
-});
+var titleTime,OriginTitile=document.title;document.addEventListener("visibilitychange",(function(){document.hidden?(document.title="w(ﾟДﾟ)w 你在看哪里？",clearTimeout(titleTime)):(document.title="♪(^∇^*)继续继续！"+OriginTitile,titleTime=setTimeout((function(){document.title=OriginTitile}),2e3))}));
